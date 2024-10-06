05d6b320417c567582d253d87227c7610f44c63d
Chrisen Galang <chrisengalang@Chrisens-MacBook-Pro.local>
2024-10-05 21:37:43 -0700

    Fix layout for sign in

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,6 +3,7 @@ import { auth } from '../firebase';
 import { GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../context/AuthContext';
+import icon from '../../public/google_sign_in.png'
 
 const Login = () => {
 
@@ -27,37 +28,47 @@ const Login = () => {
   }, [user])
 
   return (
-    <div>
-      <div>
-        <h1>Sign in</h1>
-        <div>
-          <label>Email address</label>
+    <div className='min-w-screen min-h-screen bg-slate-100 flex justify-center items-center'>
+      <div className='p-6 border rounded-lg shadow w-2/5 bg-white'>
+        <h1 className='text-center text-5xl font-bebas'>TODO APP</h1>
+        <h2 className='text-center mb-6 italic'>chrisen.dev</h2>
+        <div className='mb-3'>
+          <label className='block'>Email address</label>
           <input
+            className='px-4 py-2 border w-full'
             type='text'
             value={email}
             placeholder='Enter email address'
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-        <div>
-          <label>Password</label>
+        <div className='mb-6'>
+          <label className='block'>Password</label>
           <input
+            className='px-4 py-2 border w-full'
             type='password'
             value={password}
-            placeholder='Enter email address'
+            placeholder='Enter password'
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <div>
+        <div className='mb-6'>
           <button
+            className='p-2 bg-blue-600 hover:bg-blue-700 rounded-md text-white w-full'
             onClick={handleSignIn}>
             Sign in
           </button>
         </div>
-        <div>
+        <div className="relative flex pb-5 items-center">
+          <div className="flex-grow border-t border-gray-400"></div>
+          <span className="flex-shrink mx-4 text-gray-400">or</span>
+          <div className="flex-grow border-t border-gray-400"></div>
+        </div>
+        <div className='w-full flex justify-center'>
           <button
+            className='border border-black font-bold rounded-full shadow'
             onClick={handleGoogleSignIn}>
-            Sign in with Google
+              <img src={icon} className='h-10 hover:' />
           </button>
         </div>
       </div>
